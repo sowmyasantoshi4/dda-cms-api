9265a2735202141adf154687ad1679586e94376b
sowmya <sowmyasantoshi4@gmail.com>
2024-10-14 22:36:37 +0530

    added swagger ui

--- src/main/java/com/cms/dda/config/SwaggerConfig.java
+++ src/main/java/com/cms/dda/config/SwaggerConfig.java
@@ -1,27 +1,19 @@
 package com.cms.dda.config;
 
-/*
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
 @Configuration
-public class SwaggerConfig implements WebMvcConfigurer  {                                    
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-          		.addResourceLocations("classpath:/META-INF/resources/");
+public class SwaggerConfig {
 
-        registry.addResourceHandler("/webjars/**")
-          		.addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
-    
-    
-    
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors().and()
-                .authorizeRequests().antMatchers("/actuator/**").permitAll()
-                .and().authorizeRequests().antMatchers("/swagger-ui/**").permitAll()
-                .and().authorizeRequests().antMatchers("/v3/api-docs/**").permitAll();
-        return http.build();
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Courier Management System API")
+                                .description("API documentation for the Courier Management System")
+                                .version("v1.0.0"))
+                				.components(null);  // This will hide the schema section from the UI;
+    }
 }
-
-}
-*/
